5c34d7d6d80073e842335a3c15f2709a416f6543
kinduff <abarcadabra@gmail.com>
2015-03-21 15:02:24 -0300

    Adds default gift card data before create if data is blank

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -2,4 +2,27 @@ class Spree::GiftCard < ActiveRecord::Base
   belongs_to :product, class_name: 'Spree::Product'
 
   store :data
+
+  before_create :add_default_data
+
+  GIFT_CARD_DEFAULT_VALUES = {
+    :name => {
+      :label => 'Recipient\'s name',
+      :value => ''
+    },
+    :email => {
+      :label => 'Recipient\'s email',
+      :value => ''
+    },
+    :message => {
+      :label => 'Special gift message',
+      :value => ''
+    }
+  }
+
+  def add_default_data
+    if self.data.blank?
+      self.data = GIFT_CARD_DEFAULT_VALUES
+    end
+  end
 end
--- spec/models/spree/gift_card_spec.rb
+++ spec/models/spree/gift_card_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 
 RSpec.describe Spree::GiftCard, type: :model do
   let(:gift_card) { create :gift_card }
+  let(:custom_data) { {:custom => 'data'} }
+  let(:gift_card_custom_data) { create :gift_card, data: custom_data }
 
   it "belongs to a product" do
     expect(gift_card.product.present?).to be_truthy
+  end
+
+  it "adds data before create when empty data" do
+    expect(gift_card).to receive(:add_default_data)
+    expect(gift_card.data).to_not be_empty
+    gift_card.run_callbacks(:create)
+  end
+
+  it "assigns custom data when create" do
+    expect(gift_card_custom_data).to_not receive(:add_default_data)
+    expect(gift_card_custom_data.data.deep_symbolize_keys).to eq(custom_data)
   end
 end
