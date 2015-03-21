1346d4f83e0f69eecdf8fe25d98f2072d00a3c4f
kinduff <abarcadabra@gmail.com>
2015-03-21 19:38:16 -0300

    Adds fix_data method to gift card to sanatize and fix label and values

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -4,6 +4,7 @@ class Spree::GiftCard < ActiveRecord::Base
   store :data
 
   before_create :add_default_data
+  before_save :fix_data
 
   GIFT_CARD_DEFAULT_VALUES = {
     :name => {
@@ -24,5 +25,15 @@ class Spree::GiftCard < ActiveRecord::Base
     if self.data.blank?
       self.data = GIFT_CARD_DEFAULT_VALUES
     end
+  end
+
+  def fix_data
+    new_data = self.data.deep_symbolize_keys
+    new_data.each do |key, value|
+      if value.is_a? String or !(value.keys - [:label, :value]).blank?
+        new_data[key] = {:label => key.to_s.titleize, :value => ''} 
+      end
+    end
+    self.data = new_data
   end
 end
--- spec/models/spree/gift_card_spec.rb
+++ spec/models/spree/gift_card_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 RSpec.describe Spree::GiftCard, type: :model do
   let(:gift_card) { create :gift_card }
-  let(:custom_data) { {:custom => 'data'} }
+  let(:custom_data) { {:foo => 'bar'} }
+  let(:good_custom_data) { {:foo => {:label => 'Foo', :value => 'Bar'} } }
   let(:gift_card_custom_data) { create :gift_card, data: custom_data }
+  let(:gift_card_good_custom_data) { create :gift_card, data: good_custom_data }
 
   it "belongs to a product" do
     expect(gift_card.product.present?).to be_truthy
@@ -17,6 +19,23 @@ RSpec.describe Spree::GiftCard, type: :model do
 
   it "assigns custom data when create" do
     expect(gift_card_custom_data).to_not receive(:add_default_data)
-    expect(gift_card_custom_data.data.deep_symbolize_keys).to eq(custom_data)
+    expect(gift_card_custom_data.data.deep_symbolize_keys.keys).to eq(custom_data.keys)
+  end
+
+  context :fix_data do
+    it "fixes data before save" do
+      expect(gift_card).to receive(:fix_data)
+      expect(gift_card.data).to_not be_empty
+      gift_card.run_callbacks(:save)
+    end
+
+    it "fixes data adding label and value to missing keys" do
+      result = {:foo => {:label => 'Foo', :value => ''}}
+      expect(gift_card_custom_data.data.deep_symbolize_keys).to eq(result)
+    end
+
+    it "leaves key as is if no missing keys" do
+      expect(gift_card_good_custom_data.data.deep_symbolize_keys).to eq(good_custom_data)
+    end
   end
 end
