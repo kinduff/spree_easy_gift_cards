6bdaf7e50f24e07d98c3b881fb8001a919b7517e
kinduff <abarcadabra@gmail.com>
2015-03-22 23:53:51 -0300

    Mods data fix in giftcard humanizing keys for labels and checking for nil value keys

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -7,18 +7,9 @@ class Spree::GiftCard < ActiveRecord::Base
   before_save :fix_data
 
   GIFT_CARD_DEFAULT_VALUES = {
-    :name => {
-      :label => 'Recipient\'s name',
-      :value => ''
-    },
-    :email => {
-      :label => 'Recipient\'s email',
-      :value => ''
-    },
-    :message => {
-      :label => 'Special gift message',
-      :value => ''
-    }
+    :recipient_name => nil,
+    :recipient_email => nil,
+    :message => nil
   }
 
   def add_default_data(reset=false)
@@ -30,8 +21,8 @@ class Spree::GiftCard < ActiveRecord::Base
   def fix_data
     new_data = self.data.deep_symbolize_keys
     new_data.each do |key, value|
-      if value.is_a? String or !(value.keys - [:label, :value]).blank?
-        new_data[key] = {:label => key.to_s.titleize, :value => ''} 
+      if value.is_a? String or value.nil? or !(value.keys - [:label, :value]).blank?
+        new_data[key] = {:label => key.to_s.humanize, :value => ''}
       end
     end
     self.data = new_data
