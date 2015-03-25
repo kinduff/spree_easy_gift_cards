b4023bf36774a5879624b4c13bfc97ed3ef47a8d
kinduff <abarcadabra@gmail.com>
2015-03-25 16:12:11 -0300

    Refactors order contents add method for gift card items

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,20 +1,18 @@
 Spree::OrderContents.class_eval do
-  # Update with 3-0-stable
-  def add_to_line_item(variant, quantity, options = {})
-    line_item = grab_line_item_by_variant(variant, false, options)
+  def add(variant, quantity = 1, options = {})
+    line_item = variant.gift_card? ? add_gift_card_item(variant, quantity, options) : add_to_line_item(variant, quantity, options)
+    after_add_or_remove(line_item, options)
+  end
 
-    if line_item and !variant.gift_card?
-      line_item.quantity += quantity.to_i
-      line_item.currency = currency unless currency.nil?
-    else
+  private
+    # Update with 3-0-stable
+    def add_gift_card_item(variant, quantity, options = {})
       opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
                                           permit(Spree::PermittedAttributes.line_item_attributes)
       line_item = order.line_items.new(quantity: quantity,
                                         variant: variant,
                                         options: opts)
+      line_item.save!
+      line_item
     end
-    line_item.target_shipment = options[:shipment] if options.has_key? :shipment
-    line_item.save!
-    line_item
-  end
 end
--- spec/models/spree/order_contents_decorator_spec.rb
+++ spec/models/spree/order_contents_decorator_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Spree::OrderContents, type: :model do
   let(:order) { Spree::Order.create }
   let(:product) { create(:product) }
   let(:variant) { create(:variant, product: product) }
+  let(:regular_product) { create(:product) }
+  let(:regular_variant) { create(:variant, product: regular_product) }
 
   subject { described_class.new(order) }
 
@@ -17,6 +19,13 @@ RSpec.describe Spree::OrderContents, type: :model do
       line_item = subject.add(variant, 1)
       expect(line_item.quantity).to eq(1)
       expect(order.line_items.size).to eq(2)
+    end
+
+    it 'should update same line item if variant responds false to gift_card?' do
+      subject.add(regular_variant, 1)
+      line_item = subject.add(regular_variant, 1)
+      expect(line_item.quantity).to eq(2)
+      expect(order.line_items.size).to eq(1)
     end
   end
 end
