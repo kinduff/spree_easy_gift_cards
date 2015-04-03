a49bd59bb726aad3250fd4bb103795bf5343c61c
kinduff <abarcadabra@gmail.com>
2015-04-03 14:03:49 -0600

    Removes default data and data fix for spree gift cards

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -5,29 +5,4 @@ class Spree::GiftCard < ActiveRecord::Base
   belongs_to :line_item, class_name: 'Spree::LineItem'
 
   store :data
-
-  before_create :add_default_data
-  before_save :fix_data
-
-  GIFT_CARD_DEFAULT_VALUES = {
-    :recipient_name => nil,
-    :recipient_email => nil,
-    :message => nil
-  }
-
-  def add_default_data(reset=false)
-    if reset or self.data.blank?
-      self.data = GIFT_CARD_DEFAULT_VALUES
-    end
-  end
-
-  def fix_data
-    new_data = self.data.deep_symbolize_keys
-    new_data.each do |key, value|
-      if value.is_a? String or value.nil? or !(value.keys - [:label, :value]).blank?
-        new_data[key] = {:label => key.to_s.humanize, :value => ''}
-      end
-    end
-    self.data = new_data
-  end
 end
--- spec/models/spree/gift_card_spec.rb
+++ spec/models/spree/gift_card_spec.rb
@@ -25,39 +25,4 @@ RSpec.describe Spree::GiftCard, type: :model do
   it "belongs to a line_item" do
     expect(gift_card.line_item).to eq(line_item)
   end
-
-  context :add_default_data do
-    it "adds data before create when empty data" do
-      expect(gift_card).to receive(:add_default_data)
-      expect(gift_card.data).to_not be_empty
-      gift_card.run_callbacks(:create)
-    end
-
-    it "assigns custom data when create" do
-      expect(gift_card_custom_data).to_not receive(:add_default_data)
-      expect(gift_card_custom_data.data.deep_symbolize_keys.keys).to eq(custom_data.keys)
-    end
-
-    it "resets to default data if reset is true" do
-      gift_card_custom_data.add_default_data(true)
-      expect(gift_card_custom_data.data).to eq(gift_card.data)
-    end
-  end
-
-  context :fix_data do
-    it "fixes data before save" do
-      expect(gift_card).to receive(:fix_data)
-      expect(gift_card.data).to_not be_empty
-      gift_card.run_callbacks(:save)
-    end
-
-    it "fixes data adding label and value to missing keys" do
-      result = {:foo => {:label => 'Foo', :value => ''}}
-      expect(gift_card_custom_data.data.deep_symbolize_keys).to eq(result)
-    end
-
-    it "leaves key as is if no missing keys" do
-      expect(gift_card_good_custom_data.data.deep_symbolize_keys).to eq(good_custom_data)
-    end
-  end
 end
