1b9be18eca8262e48ef970bde9f82292e3707cd1
kinduff <abarcadabra@gmail.com>
2015-04-07 17:05:14 -0500

    Adds order and user method to gift_card

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -2,4 +2,12 @@ class Spree::GiftCard < ActiveRecord::Base
   belongs_to :line_item, class_name: 'Spree::LineItem'
 
   store :data
+
+  def order
+    line_item.order
+  end
+
+  def user
+    order.user
+  end
 end
