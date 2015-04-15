f1e5c376df76db1dd0032e10ec42c3cb04e59a04
kinduff <abarcadabra@gmail.com>
2015-04-15 01:32:07 -0500

    Mods and refactors gift card email

--- app/mailers/spree/gift_card_mailer.rb
+++ app/mailers/spree/gift_card_mailer.rb
@@ -2,9 +2,10 @@ module Spree
   class GiftCardMailer < BaseMailer
     def gift_card_email(gift_card, resend = false)
       @gift_card = gift_card
+      @order = @gift_card.order
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "#{Spree::Store.current.name} #{Spree.t('gift_card_mailer.gift_card_email.subject')}"
-      mail(to: @gift_card.recipient_email, subject: subject)
+      mail(to: @gift_card.recipient_email, from: from_address, subject: subject)
     end
   end
 end
