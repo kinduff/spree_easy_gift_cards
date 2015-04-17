9845ac0b0e59923fdfa827140debc80a3fdef95f
kinduff <abarcadabra@gmail.com>
2015-04-17 13:20:15 -0500

    Runs gift card product generator on install

--- lib/generators/spree_easy_gift_cards/install/install_generator.rb
+++ lib/generators/spree_easy_gift_cards/install/install_generator.rb
@@ -31,6 +31,19 @@ module SpreeEasyGiftCards
       def copy_initializer
         copy_file "spree_easy_gift_cards.rb", "config/initializers/spree_easy_gift_cards.rb"
       end
+
+      def generate_gift_card_product
+        puts "Initial Gift Card Setup contains:"
+        puts "+ Digital Shipment category and method"
+        puts "+ Gift Card Product"
+        puts "+ Gift Card Variants (10,20, 50,100,200)"
+        run_generator = ['', 'y', 'Y'].include?(ask 'Would you like to install the initial Gift Card setup? [Y/n]')
+        if run_generator
+          generate 'spree_easy_gift_cards:gift_card_product'
+        else
+          puts 'Skipping generator spree_easy_gift_cards:gift_card_product, make sure you know what you\'re doing!'
+        end
+      end
     end
   end
 end
